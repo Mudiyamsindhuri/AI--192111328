f828f72486b8d71d388c1422f25c6edf56c8b150
Mudiyamsindhuri <112714336+Mudiyamsindhuri@users.noreply.github.com>
2023-11-29 13:19:09 +0530

    Add files via upload

---
+++ date of birth.pl
@@ -0,0 +1,6 @@
+born(jasmitha, 14,10,2003).
+born(reshmitha, 04,10,2009).
+born(gowtham, 02,10,2003).
+born(prudhvi, 20,10,2003).
+born(yaswanth, 04,10,2009).
+born(leo, 26,08,2001).
---
+++ student-teacher.pl
@@ -0,0 +1,5 @@
+takes(prasoona, eca01).
+takes(prasoona, csa45).
+takes(rishitha, ita02).
+takes(rishitha, uba54).
+classmates(X, Y) :- takes(X, Z), takes(Y, Z).
